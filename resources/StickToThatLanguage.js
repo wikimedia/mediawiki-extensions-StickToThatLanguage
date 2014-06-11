9d98ab7dae7e43182be441b8aaaa533cf40d61d0
Timo Tijhof <krinklemail@gmail.com>
2014-06-11 15:55:18 +0200

    Clean up CSS and JS
    * Pass css selector to prependTo() instead of constructing the
      object manually only to have jQuery make a clone of it.
      Either use $(selector).prepend() or prependTo(selector), doing
      both creates additional objects for no reason.
    
    * Use .eq(i) instead of weird double-constructor pattern:
      > $( $( '#p-lang .body ul' )[1] )
      I've never seen this before and no idea where this pattern
      originates from or why anyone would do that.
    
    * Minor style changes to improve readability and conformance
      with MediaWiki coding style:
      - Indent chained methods calls (firstly for consistency,
        secondly because one could argue it makes it clearer which
        statements are siblings and which are continued statements,
        and clarifies how many arguments are passed to a method)
      - Avoid trailing line comments.
    
    * Remove slash from element creation construction. $() tags
      a tag description (to create an element from) or valid html
      to parse using the parser. <a/> is neither valid html (<a></a>)
      nor a simple tag (<a>). It works due to fault tolerance, but
      is considered bad practice.
    
    * Add .jshintrc file to avoid error for undefined jQuery/mediaWiki.
    
    Per https://www.mediawiki.org/wiki/CC/JS
    
    Change-Id: Ibbb5c2db7865b43e6625aaa9a49fde5d8cb7cbe9

--- resources/StickToThatLanguage.js
+++ resources/StickToThatLanguage.js
@@ -1,60 +1,61 @@
 /**
- * JavaScript for 'StickToThatLanguage' extension
+ * JavaScript for the StickToThatLanguage extension
  *
  * @since 0.1
- * @file StickToThatLanguage.js
+ * @file
  * @ingroup STTLanguage
  *
  * @licence GNU GPL v2+
  * @author H. Snater
  */
 
-( function( $, mw ) {
+( function ( $, mw ) {
 	'use strict';
 
-	$( document ).ready( function() {
-		// get user preferred languages in the DOM:
-		var topLanguages = $( '.sttl-toplang' );
+	$( function () {
+		// Get user preferred languages in the DOM:
+		var $topLanguages = $( '.sttl-toplang' );
 
-		if( topLanguages.length === 0 ) {
-			// no 'more languages' if there is no languages displayed at all
+		if ( !$topLanguages.length ) {
+			// No 'more languages' if there is no languages displayed at all
 			return;
 		}
 
-		// place linked separator to have other languages collapse below top 10 languages
-		topLanguages
-		.removeClass( 'sttl-lasttoplang' ) // remove non-JS exclusive class
-		.detach()
-		.appendTo(
-			$( '<ul/>' ).prependTo(
-				$( '#p-lang .body' )
-			) // put top languages into its own ul and append link to show more languages
-			.after(
-				$( '<h6/>' )
-				.append(
-					$( '<span/>' )
-					.addClass( 'ui-icon ui-icon-triangle-1-e' )
-				)
-				.append(
-					$( '<a/>' )
-					.addClass( 'sttl-languages-more-link' )
-					.text( mw.msg( 'sttl-languages-more-link' ) )
-					.attr( 'href', '#' )
-					.click( function( event ) {
-						event.preventDefault();
-						$( '#p-lang .body h6 span' )
-						.toggleClass( 'ui-icon-triangle-1-e' )
-						.toggleClass( 'ui-icon-triangle-1-s' );
-						$( $( '#p-lang .body ul' )[1] ).slideToggle();
-					} )
-				)
-			)
-		);
+		// Place linked separator to have other languages collapse below top 10 languages
+		$topLanguages
+			// Remove non-JS exclusive class
+			.removeClass( 'sttl-lasttoplang' )
+			.detach()
+			.appendTo(
+				$( '<ul>' )
+					.prependTo( '#p-lang .body' )
+					// Put top languages into its own ul and append link to show more languages
+					.after(
+						$( '<h6>' )
+							.append(
+								$( '<span>' )
+									.addClass( 'ui-icon ui-icon-triangle-1-e' )
+							)
+							.append(
+								$( '<a>' )
+									.addClass( 'sttl-languages-more-link' )
+									.text( mw.msg( 'sttl-languages-more-link' ) )
+									.attr( 'href', '#' )
+									.click( function ( e ) {
+										e.preventDefault();
+										$( '#p-lang .body h6 span' )
+											.toggleClass( 'ui-icon-triangle-1-e' )
+											.toggleClass( 'ui-icon-triangle-1-s' );
+										$( '#p-lang .body ul' ).eq( 1 ).slideToggle();
+									} )
+							)
+					)
+			);
 
 		// class style initially hides "more" languages
-		$( $( '#p-lang .body ul' )[1] ).addClass( 'sttl-languages-more' );
+		$( '#p-lang .body ul' ).eq( 1 ).addClass( 'sttl-languages-more' );
 
 	});
 
-} )( jQuery, mediaWiki );
+}( jQuery, mediaWiki ) );
 
